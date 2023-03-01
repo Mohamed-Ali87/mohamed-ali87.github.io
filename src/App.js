e7f9fae00305621815982763a6b904d9d432179b
mohamed-ali87 <mohamed.ali200187@gmail.com>
2023-03-01 18:48:29 -0500

    adding github link

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Mohamed-Ali87"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My github
         </a>
       </header>
     </div>
